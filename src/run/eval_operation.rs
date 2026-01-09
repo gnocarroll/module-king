086bd636b0e7760ace3a5b24b2cd7738f18cd5ef
gnocarroll <george.ocarroll@comcast.net>
2026-01-09 14:45:35 -0500

    working on execution of binary ops in interpreter

--- src/run/eval_operation.rs
+++ src/run/eval_operation.rs
@@ -1,7 +1,14 @@
+use std::ops::{Add, Div, Mul, Rem, Sub};
+
 use crate::{
-    parse::{AST, ExprVariant, Operation, TypeOrModule, ast_contents::ExprID},
+    parse::{
+        AST, ExprVariant, Operation, TypeOrModule,
+        ast_contents::{ExprID, TypeID},
+    },
     run::{
-        ExecutionContext, Value, ValueVariant, error::{RuntimeError, RuntimeErrorVariant}, eval
+        ExecutionContext, Value, ValueVariant,
+        error::{RuntimeError, RuntimeErrorVariant},
+        eval,
     },
     scan::TokenType,
 };
@@ -31,7 +38,10 @@ fn eval_operation_unary(
     op: TokenType,
     operand: ExprID,
 ) -> Result<Value, RuntimeError> {
-    let invalid_op = Err(RuntimeError { expr, variant: RuntimeErrorVariant::InvalidOperation });
+    let invalid_op = Err(RuntimeError {
+        expr,
+        variant: RuntimeErrorVariant::InvalidOperation,
+    });
 
     let type_id = match ast.objs.expr(operand).type_or_module {
         TypeOrModule::Type(t) => t,
@@ -42,18 +52,26 @@ fn eval_operation_unary(
     let operand_value = eval(ast, ctx, operand)?;
 
     let ret = match (op, &operand_value.variant) {
+        // unary +
         (TokenType::Plus, ValueVariant::Integer(_) | ValueVariant::Float(_)) => {
             // no-op
             return Ok(operand_value);
         }
+
+        // unary -
         (TokenType::Minus, ValueVariant::Integer(i)) => ValueVariant::Integer(-i),
         (TokenType::Minus, ValueVariant::Float(f)) => ValueVariant::Float(-f),
+
+        // unrecognized
         _ => {
             return invalid_op;
         }
     };
 
-    Ok(Value { type_id: Some(type_id), variant: ret })
+    Ok(Value {
+        type_id: Some(type_id),
+        variant: ret,
+    })
 }
 
 fn eval_operation_binary(
@@ -64,5 +82,53 @@ fn eval_operation_binary(
     operand1: ExprID,
     operand2: ExprID,
 ) -> Result<Value, RuntimeError> {
-    Err(RuntimeError { expr, variant: RuntimeErrorVariant::NotImplemented })
+    let invalid_op = Err(RuntimeError {
+        expr,
+        variant: RuntimeErrorVariant::InvalidOperation,
+    });
+
+    let type_ids = [
+        match ast.objs.expr(operand1).type_or_module {
+            TypeOrModule::Type(t) => t,
+            TypeOrModule::Module(_) => return invalid_op,
+        },
+        match ast.objs.expr(operand2).type_or_module {
+            TypeOrModule::Type(t) => t,
+            TypeOrModule::Module(_) => return invalid_op,
+        },
+    ];
+
+    let operand_values = [eval(ast, ctx, operand1)?, eval(ast, ctx, operand2)?];
+
+    let i64_pow = |lhs: i64, rhs: i64| lhs.pow(rhs as u32);
+
+    let integer_op = |ttype: TokenType| -> Option<fn(i64, i64) -> i64> {
+        match ttype {
+            TokenType::Plus => Some(i64::add),
+            TokenType::Minus => Some(i64::sub),
+            TokenType::Star => Some(i64::mul),
+            TokenType::FSlash => Some(i64::div),
+            TokenType::Percent => Some(i64::rem),
+            TokenType::StarStar => Some(i64_pow),
+            _ => None,
+        }
+    };
+
+    let ret = match (&operand_values[0].variant, &operand_values[1].variant) {
+        (ValueVariant::Integer(lhs), ValueVariant::Integer(rhs)) => {
+            if let Some(i64_op) = integer_op(op) {
+                ValueVariant::Integer(i64_op(*lhs, *rhs))
+            } else {
+                return invalid_op;
+            }
+        }
+        _ => {
+            return invalid_op;
+        }
+    };
+
+    Ok(Value {
+        type_id: Some(type_ids[0]),
+        variant: ret,
+    })
 }
