82db2533ac4532e932ba7b4b1b536c2317678217
gnocarroll <george.ocarroll@comcast.net>
2026-01-19 10:12:58 -0500

    working on applying functions/type cast

--- src/run/eval_operation/apply.rs
+++ src/run/eval_operation/apply.rs
@@ -1,14 +1,14 @@
 use crate::{
     parse::{
         AST,
-        ast_contents::{ExprID, FunctionID},
+        ast_contents::{ExprID, FunctionID, TypeID},
     },
     run::{
         ExecutionContext,
-        context_contents::{RuntimeReference, ValueVariant},
+        context_contents::RuntimeReference,
         error::{RuntimeErrorVariant, RuntimeException},
         eval, expr_to_unit,
-        util::allocate_instances_from_pattern,
+        util::{allocate_instances_from_pattern, runtime_ref_to_function, runtime_ref_to_type},
     },
 };
 
@@ -19,17 +19,18 @@ pub fn eval_operation_apply(
     operand1: ExprID,
     operand2: ExprID,
 ) -> Result<RuntimeReference, RuntimeException> {
-    let func = eval(ast, ctx, operand1)?;
+    let runtime_ref = eval(ast, ctx, operand1)?;
 
-    match ctx.objs.ref_get(func).variant {
-        ValueVariant::Function(function_id) => {
-            eval_operation_apply_function(ast, ctx, expr, function_id, operand2)
-        }
-        _ => Err(RuntimeException {
-            expr,
-            variant: RuntimeErrorVariant::InvalidOperation,
-        }),
+    if let Some(function_id) = runtime_ref_to_function(ast, ctx, runtime_ref) {
+        return eval_operation_apply_function(ast, ctx, expr, function_id, operand2);
+    } else if let Some(type_id) = runtime_ref_to_type(ast, ctx, runtime_ref) {
+        return eval_operation_apply_cast(ast, ctx, expr, type_id, operand2);
     }
+
+    return Err(RuntimeException {
+        expr,
+        variant: RuntimeErrorVariant::InvalidOperation,
+    });
 }
 
 fn eval_operation_apply_function(
@@ -47,7 +48,19 @@ fn eval_operation_apply_function(
         allocate_instances_from_pattern(ast, ctx, *pattern_id, function_struct.scope);
     }
 
-    let new_value_ref = eval(ast, ctx, operand2)?;
+    let args_ref = eval(ast, ctx, args)?;
+
+    Ok(expr_to_unit(ast, ctx, expr))
+}
+
+fn eval_operation_apply_cast(
+    ast: &AST,
+    ctx: &mut ExecutionContext,
+    expr: ExprID,
+    type_id: TypeID,
+    args: ExprID,
+) -> Result<RuntimeReference, RuntimeException> {
+    // TODO: implement
 
     Ok(expr_to_unit(ast, ctx, expr))
 }
